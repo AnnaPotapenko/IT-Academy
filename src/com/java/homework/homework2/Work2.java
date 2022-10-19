db04c004ddec64d40077c3d611c8c1f199901cd4
AnnaPotapenko <nyuta.skachko@mail.ru>
2022-10-19 13:39:35 +0300

    дз

---
+++ src/com/java/homework/homework2/Work2.java
@@ -0,0 +1,9 @@
+package com.java.homework.homework2;
+
+public class Work2 {
+    public static void main(String[]args){
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+
+    }
+}
