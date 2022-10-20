d896da6e7a6c3d15b7071dfb82f885ab2123fbd3
AnnaPotapenko <nyuta.skachko@mail.ru>
2022-10-20 14:57:04 +0300

    Выполненное дз по лекции 2

---
+++ src/com/java/homework/homework2/ClassOne.java
@@ -0,0 +1,56 @@
+package com.java.homework.homework2;
+
+public class ClassOne {
+    public static void main(String[]args){
+        //Создаем локальные переменные используя поля класса2
+        ClassTwo MyClass = new ClassTwo();
+        MyClass.setMyByte((byte)-5); // Пришлось привести к байту, т.к. значение в классе указано как байт, а просто число 5 это инт.
+        MyClass.setMyShort((byte)32000);
+        MyClass.setMyInt(1000000);
+        MyClass.setMyLong(5215468452154584L);
+        MyClass.setMyFloat(2.5F);
+        MyClass.setMyDouble(2594.26547);
+        MyClass.setMyChar('\u0021');
+        MyClass.setMyBoolean(false);
+
+        MyClass.setMyByteWrapper((byte)8);
+        MyClass.setMyShortWrapper((short)15000);
+        MyClass.setMyIntWrapper(25648651);
+        MyClass.setMyLongWrapper(-16468451654L);
+        MyClass.setMyFloatWrapper(-15.2F);
+        MyClass.setMyDoubleWrapper(154.2);
+        MyClass.setMyCharWrapper('\u0024');
+        MyClass.setMyBooleanWrapper(true);
+
+        MyClass.setMyString("Это строка");
+        // Вывод на консоль
+        System.out.println("Выводим на консоль примитивные переменные:");
+        System.out.println("byte = "+ MyClass.getMyByte());
+        System.out.println("short = " + MyClass.getMyShort());
+        System.out.println("int = " + MyClass.getMyInt());
+        System.out.println("long = " + MyClass.getMyLong());
+        System.out.println("float = " + MyClass.getMyFloat());
+        System.out.println("double = " + MyClass.getMyDouble());
+        System.out.println("char is " + MyClass.getMyChar());
+        System.out.println("boolean is " + MyClass.getMyBoolean());
+        System.out.println("Выводим на консоль обёртки:");
+        System.out.println("Byte = " + MyClass.getMyByteWrapper());
+        System.out.println("Short = " + MyClass.getMyShortWrapper());
+        System.out.println("Integer = " + MyClass.getMyIntWrapper());
+        System.out.println("Long = " + MyClass.getMyLongWrapper());
+        System.out.println("Float = "+ MyClass.getMyFloatWrapper());
+        System.out.println("Double = " + MyClass.getMyDoubleWrapper());
+        System.out.println("Character is " + MyClass.getMyCharWrapper());
+        System.out.println("Boolean is " + MyClass.getMyBooleanWrapper());
+        System.out.println("Вывод строки:");
+        System.out.println("String: " + MyClass.getMyString());
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/java/homework/homework2/ClassTwo.java
@@ -0,0 +1,145 @@
+package com.java.homework.homework2;
+
+public class ClassTwo {
+    // Создание глобальных примитивных переменных
+    private byte myByte;
+    private short myShort;
+    private int myInt;
+    private long myLong;
+    private float myFloat;
+    private double myDouble;
+    private char myChar;
+    private boolean myBoolean;
+    // Создание геттеров и сеттеров для примитивных переменных
+    public void setMyByte (byte personalByte){
+        myByte = personalByte;
+    }
+    public byte getMyByte(){
+        return myByte;
+    }
+    public void setMyShort(short personalByte){
+        myShort = personalByte;
+    }
+    public short getMyShort() {
+        return myShort;
+    }
+    public void setMyInt (int personalInt){
+        myInt = personalInt;
+    }
+    public int getMyInt() {
+        return myInt;
+    }
+    public void setMyLong(long personalLong) {
+        myLong = personalLong;
+    }
+
+    public long getMyLong() {
+        return myLong;
+    }
+    public void setMyFloat(float personalFloat){
+        myFloat = personalFloat;
+    }
+
+    public float getMyFloat() {
+        return myFloat;
+    }
+    public void setMyDouble(double personalDouble){
+        myDouble=personalDouble;
+    }
+
+    public double getMyDouble() {
+        return myDouble;
+    }
+    public void setMyChar (char personalChar){
+        myChar = personalChar;
+    }
+
+    public char getMyChar() {
+        return myChar;
+    }
+
+    public void setMyBoolean(Boolean personalBoolean) {
+        myBoolean = personalBoolean;
+    }
+    public boolean getMyBoolean(){
+        return myBoolean;
+    }
+
+    // Создание глобальных оберток
+    private Byte myByteWrapper;
+    private Short myShortWrapper;
+    private Integer myIntWrapper;
+    private Long myLongWrapper;
+    private Float myFloatWrapper;
+    private Double myDoubleWrapper;
+    private Character myCharWrapper;
+    private Boolean myBooleanWrapper;
+    // Создание сеттеров и геттеров для оберток
+
+    public void setMyByteWrapper(Byte personalByteWrap){
+        myByteWrapper = personalByteWrap;
+    }
+    public Byte getMyByteWrapper() {
+        return myByteWrapper;
+    }
+    public void setMyShortWrapper (Short personalShortWrap){
+        myShortWrapper =personalShortWrap;
+    }
+    public Short getMyShortWrapper(){
+        return myShortWrapper;
+    }
+    public void setMyIntWrapper (Integer personalIntWrap){
+        myIntWrapper = personalIntWrap;
+    }
+    public Integer getMyIntWrapper (){
+        return myIntWrapper;
+    }
+    public void setMyLongWrapper (Long personalLongWrap){
+        myLongWrapper = personalLongWrap;
+    }
+    public Long getMyLongWrapper(){
+        return myLongWrapper;
+    }
+    public void setMyFloatWrapper (Float personalFloatWrap){
+        myFloatWrapper = personalFloatWrap;
+    }
+    public Float getMyFloatWrapper(){
+        return myFloatWrapper;
+    }
+    public void setMyDoubleWrapper(Double personalDoubleWrap){
+        myDoubleWrapper = personalDoubleWrap;
+    }
+
+    public Double getMyDoubleWrapper() {
+        return myDoubleWrapper;
+    }
+    public void setMyCharWrapper(Character personalCharWrap){
+        myCharWrapper = personalCharWrap;
+    }
+    public Character getMyCharWrapper(){
+        return myCharWrapper;
+    }
+
+    public void setMyBooleanWrapper(Boolean personalBooleanWrap) {
+        myBooleanWrapper = personalBooleanWrap;
+    }
+    public Boolean getMyBooleanWrapper(){
+        return myBooleanWrapper;
+    }
+
+    // Создание строкового типа данных
+    private String myString;
+    // Создание геттера и сеттера для строки
+    public void setMyString(String personalString){
+        myString = personalString;
+    }
+    public String getMyString(){
+        return myString;
+    }
+
+
+
+
+
+
+}
