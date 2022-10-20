2bf291dd73abb528e940375c224568e553b11678
AnnaPotapenko <nyuta.skachko@mail.ru>
2022-10-20 15:17:58 +0300

    Лекция 3

--- src/com/java/lessons/lesson3/Lesson3.java
+++ src/com/java/lessons/lesson3/Lesson3.java
@@ -2,6 +2,6 @@ package com.java.lessons.lesson3;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        System.out.println("Лекция 3");
+        System.out.println("Лекция 3!");
     }
 }
