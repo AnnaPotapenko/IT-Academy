3bd2688aea1d272e3d69982b6f5b15839d1fa185
AnnaPotapenko <nyuta.skachko@mail.ru>
2022-10-20 14:59:28 +0300

    Лекция 2

---
+++ src/com/java/lessons/lesson2/LessonTwo.java
@@ -0,0 +1,10 @@
+package com.java.lessons.lesson2;
+
+public class LessonTwo {
+    public static void main(String[] args) {
+
+        long num = 265681945622164L;
+        
+        System.out.println(num);
+    }
+}
