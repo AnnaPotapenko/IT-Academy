967fea1e27a2ac81befdf6031e9eab5a8410e33b
AnnaPotapenko <nyuta.skachko@mail.ru>
2022-10-26 20:19:34 +0300

    Выполненное дз по лекции 4

---
+++ src/com/java/homework/homework4/Cat.java
@@ -0,0 +1,61 @@
+package com.java.homework.homework4;
+
+
+public class Cat {
+
+    private String catName = "Barsik";
+    private int catAge = 2;
+
+    public Cat() {
+
+    }
+
+    public Cat(String catName, int catAge) {
+        this.catName = catName;
+        this.catAge = catAge;
+
+    }
+
+    public void setCatAge(int catAge) {
+        this.catAge = catAge;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public int getCatAge() {
+        return this.catAge;
+    }
+
+    public String getCatName() {
+        return this.catName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        // 1
+        if (this == o) {
+            return true;
+        }
+
+        // 2
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        // 3
+        Cat cat = (Cat) o;
+        return catAge == cat.catAge &&
+                catName.equals(cat.catName);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat name is " + catName +
+                ", cat age is " + catAge;
+    }
+
+
+}
---
+++ src/com/java/homework/homework4/Main.java
@@ -0,0 +1,99 @@
+package com.java.homework.homework4;
+
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int catCount = 1;
+        int catNumberForDoWhile = 1;
+
+        System.out.println("Выводим котиков с циклом while и дефолтным значением");
+
+        while (catCount != 11) {
+            Cat cat1 = new Cat();
+            System.out.print("Cat № " + catCount + ": ");
+            System.out.println(cat1);
+            catCount++;
+        }
+
+
+        System.out.println("Выводим котиков через цикл for с рандомным именем и возрастом");
+
+        for (int i = 1; i < 11; i++) {
+            Cat cat2 = new Cat();
+            String catName = getRandomString();
+            int catAge = getRandomAge();
+
+            cat2.setCatAge(catAge);
+            cat2.setCatName(catName);
+            System.out.print("Cat № " + i + ": ");
+            System.out.println(cat2.getCatName() + cat2.getCatAge());
+        }
+
+
+        System.out.println("Выводим котиков через цикл do while с конструктором");
+
+
+        do {
+            Cat cat3 = new Cat("Polly", 11);
+            System.out.print("Cat № " + catNumberForDoWhile + ": ");
+            System.out.println(cat3);
+            catNumberForDoWhile++;
+
+
+        } while (catNumberForDoWhile != 11);
+
+
+        System.out.println("Выводим котиков через цикл foreach и массив");
+
+        Cat[] catArray = new Cat[5];
+        catArray[0] = new Cat("Billy", 6);
+        catArray[1] = new Cat("Charly", 5);
+        catArray[2] = new Cat("Barsik", 2);
+        catArray[3] = new Cat("Mary", 1);
+        catArray[4] = new Cat("Barsik", 2);
+        for (Cat catDiscription : catArray) {
+            System.out.println(catDiscription);
+        }
+
+        System.out.println("Сравним экземпляры класса Cat, находящиеся в массиве");
+
+        System.out.println(catArray[2].equals(catArray[0])); // должно быть фолс
+        System.out.println(catArray[2].equals(catArray[4])); // тру
+
+        System.out.println("Создадим три новых экземпляра и сравним их");
+
+        Cat catBarsik2 = new Cat("Barsik", 2);
+        Cat catBarsic2 = new Cat("Barsic", 2);
+
+        System.out.println(catBarsik2.equals(catArray[2])); // должно быть тру
+        System.out.println(catBarsic2.equals(catBarsik2)); // должно быть фолс
+
+
+    }
+
+    private static int getRandomAge() {
+        Random random = new Random();
+
+        return random.nextInt(28) + 1;
+
+    }
+
+
+    private static String getRandomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int stringLength = 6;
+        Random random = new Random();
+
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(stringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
